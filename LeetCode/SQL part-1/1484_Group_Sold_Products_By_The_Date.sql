95129537f7af064e450b76e397a039ffb603be53
urmisen <urmisen1202@gmail.com>
2023-01-07 23:28:58 +0600

    code added

---
+++ LeetCode/SQL part-1/1484_Group_Sold_Products_By_The_Date.sql
@@ -0,0 +1,32 @@
+-- Table Activities:
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | sell_date   | date    |
+-- | product     | varchar |
+-- +-------------+---------+
+-- There is no primary key for this table, it may contain duplicates.
+-- Each row of this table contains the product name and the date it was sold in a market.
+ 
+
+-- Write an SQL query to find for each date the number of different products sold and their names.
+
+-- The sold products names for each date should be sorted lexicographically.
+
+-- Return the result table ordered by sell_date.
+
+-- The query result format is in the following example.
+
+-- Query :
+
+SELECT 
+    sell_date,
+    COUNT(DISTINCT product) AS num_sold,
+    GROUP_CONCAT(DISTINCT product) AS products
+FROM 
+    Activities
+GROUP BY 
+    sell_date
+ORDER BY 
+    sell_date,products
--- LeetCode/SQL part-1/1667_Fix_Names_in_a_Table.sql
+++ LeetCode/SQL part-1/1667_Fix_Names_in_a_Table.sql
@@ -18,4 +18,10 @@
 
 -- Query :
 
-
+SELECT 
+     user_id, 
+     CONCAT(UPPER(SUBSTR(name, 1,1)), LOWER(SUBSTR(name,2))) AS name
+FROM 
+     users
+ORDER BY 
+     user_id
