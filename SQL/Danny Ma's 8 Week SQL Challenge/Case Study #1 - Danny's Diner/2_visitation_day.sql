2a26810cc3cd294bc086e15f482b8ad83d3813c0
Urmi  Sen <urmisen1202@gmail.com>
2023-01-18 00:40:24 +0600

    Update 2_visitation_day.sql

--- SQL/Danny Ma's 8 Week SQL Challenge/Case Study #1 - Danny's Diner/2_visitation_day.sql
+++ SQL/Danny Ma's 8 Week SQL Challenge/Case Study #1 - Danny's Diner/2_visitation_day.sql
@@ -1,11 +1,11 @@
 -- 2. How many days has each customer visited the restaurant?
 
 SELECT 
-	customer_id,
-    COALESCE(COUNT(DATE(order_date)),0) AS visited_days
+     customer_id,
+     COALESCE(COUNT(DATE(order_date)),0) AS visited_days
 FROM 
-	dannys_diner.sales
+     dannys_diner.sales
 GROUP BY 
-	1
+     1
 ORDER BY 
-	2 DESC
+     2 DESC
