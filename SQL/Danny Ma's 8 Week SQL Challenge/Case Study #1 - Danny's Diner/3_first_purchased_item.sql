fa3d3aff8b46a46c0aa18a2d998fcef469d1dcdb
Urmi  Sen <urmisen1202@gmail.com>
2023-01-18 00:39:53 +0600

    Update 3_first_purchased_item.sql

--- SQL/Danny Ma's 8 Week SQL Challenge/Case Study #1 - Danny's Diner/3_first_purchased_item.sql
+++ SQL/Danny Ma's 8 Week SQL Challenge/Case Study #1 - Danny's Diner/3_first_purchased_item.sql
@@ -2,25 +2,25 @@
 
 WITH customer_purchase_data AS
 (
-  	SELECT 
-        customer_id,
-  		product_id,
-  		product_name,
-  		MIN(order_date) AS first_purchase_date,
-        DENSE_RANK() OVER(PARTITION BY customer_id ORDER BY MIN(order_date), product_id) AS item_rank
-    FROM 
-        dannys_diner.sales
-    JOIN 
-        dannys_diner.menu
-    USING(product_id)
-  	GROUP BY 
-  		1,2,3
+SELECT 
+     customer_id,
+     product_id,
+     product_name,
+     MIN(order_date) AS first_purchase_date,
+     DENSE_RANK() OVER(PARTITION BY customer_id ORDER BY MIN(order_date), product_id) AS item_rank
+FROM 
+     dannys_diner.sales
+JOIN 
+     dannys_diner.menu
+     USING(product_id)
+GROUP BY 
+     1,2,3
 )
 
 SELECT 
-	customer_id,
-    product_name AS first_purchased_item
+     customer_id,
+     product_name AS first_purchased_item
 FROM 
-	customer_purchase_data
+     customer_purchase_data
 WHERE 
-	item_rank = 1
+     item_rank = 1
