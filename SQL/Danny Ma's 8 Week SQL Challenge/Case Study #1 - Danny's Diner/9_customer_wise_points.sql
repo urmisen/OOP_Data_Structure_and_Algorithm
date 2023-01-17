a130145befafe23b2cb685a9e0c6cb06226c4921
Urmi  Sen <urmisen1202@gmail.com>
2023-01-18 00:33:45 +0600

    Update 9_customer_wise_points.sql

--- SQL/Danny Ma's 8 Week SQL Challenge/Case Study #1 - Danny's Diner/9_customer_wise_points.sql
+++ SQL/Danny Ma's 8 Week SQL Challenge/Case Study #1 - Danny's Diner/9_customer_wise_points.sql
@@ -2,26 +2,26 @@
 
 WITH customer_purchase_data AS
 (
-  	SELECT 
-        customer_id,
-  		order_date,
-  		product_id,
-  		product_name,
-  		price
-    FROM 
-        dannys_diner.sales
-    JOIN 
-        dannys_diner.menu
-    USING(product_id)
-  	
+SELECT 
+     customer_id,
+     order_date,
+     product_id,
+     product_name,
+     price
+FROM 
+     dannys_diner.sales
+JOIN 
+     dannys_diner.menu
+USING(product_id)
+
 )
 
 SELECT 
-	customer_id,
-    SUM(CASE WHEN product_name ILIKE '%sushi%' THEN price*2*10 ELSE price*10 END) AS total_points
+     customer_id,
+     SUM(CASE WHEN product_name ILIKE '%sushi%' THEN price*2*10 ELSE price*10 END) AS total_points
 FROM 
-	customer_purchase_data
+     customer_purchase_data
 GROUP BY 
-	1
+     1
 ORDER BY
-	1
+     1
