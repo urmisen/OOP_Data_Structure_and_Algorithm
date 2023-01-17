d75a8a3d54ee1c392867544b635b9775223dd43b
Urmi  Sen <urmisen1202@gmail.com>
2023-01-18 00:38:59 +0600

    Update 4_most_purchased_item.sql

--- SQL/Danny Ma's 8 Week SQL Challenge/Case Study #1 - Danny's Diner/4_most_purchased_item.sql
+++ SQL/Danny Ma's 8 Week SQL Challenge/Case Study #1 - Danny's Diner/4_most_purchased_item.sql
@@ -2,24 +2,24 @@
 
 WITH product_sell_data AS
 (
-  	SELECT 
-  		product_id,
-  		product_name,
-  		COUNT(*) AS purchased_time,
-        DENSE_RANK() OVER(ORDER BY COUNT(*) DESC) AS item_rank
-    FROM 
-        dannys_diner.sales
-    JOIN 
-        dannys_diner.menu
-    USING(product_id)
-  	GROUP BY 
-  		1,2
+SELECT 
+     product_id,
+     product_name,
+     COUNT(*) AS purchased_time,
+     DENSE_RANK() OVER(ORDER BY COUNT(*) DESC) AS item_rank
+FROM 
+     dannys_diner.sales
+JOIN 
+     dannys_diner.menu
+USING(product_id)
+GROUP BY 
+     1,2
 )
 
 SELECT 
-	product_name AS most_purchased_item,
-    purchased_time
+     product_name AS most_purchased_item,
+     purchased_time
 FROM 
-	product_sell_data
+     product_sell_data
 WHERE 
-	item_rank = 1
+     item_rank = 1
