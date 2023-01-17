dcaeab3c18751e550516cb868537c1309ea6f388
Urmi  Sen <urmisen1202@gmail.com>
2023-01-18 00:37:00 +0600

    Update 6_first_purchase_after_joining.sql

--- SQL/Danny Ma's 8 Week SQL Challenge/Case Study #1 - Danny's Diner/6_first_purchase_after_joining.sql
+++ SQL/Danny Ma's 8 Week SQL Challenge/Case Study #1 - Danny's Diner/6_first_purchase_after_joining.sql
@@ -2,28 +2,28 @@
 
 WITH customer_purchase_data AS
 (
-  	SELECT 
-        customer_id,
-  		product_id,
-  		product_name,
-  		order_date,
-        DENSE_RANK() OVER(PARTITION BY customer_id ORDER BY DATE(order_date) ASC) AS item_rank
-    FROM 
-        dannys_diner.sales
-    JOIN 
-        dannys_diner.menu
-    USING(product_id)
-  	JOIN 
-  		dannys_diner.members 
-  	USING(customer_id)
-  	WHERE 
-  		DATE(order_date)>= DATE(join_date)
+SELECT 
+     customer_id,
+     product_id,
+     product_name,
+     order_date,
+     DENSE_RANK() OVER(PARTITION BY customer_id ORDER BY DATE(order_date) ASC) AS item_rank
+FROM 
+     dannys_diner.sales
+JOIN 
+     dannys_diner.menu
+USING(product_id)
+JOIN 
+     dannys_diner.members 
+USING(customer_id)
+WHERE 
+     DATE(order_date)>= DATE(join_date)
 )
 
 SELECT 
-	customer_id,
-    product_name AS first_purchased_item
+     customer_id,
+     product_name AS first_purchased_item
 FROM 
-	customer_purchase_data
+     customer_purchase_data
 WHERE 
-	item_rank = 1
+     item_rank = 1
